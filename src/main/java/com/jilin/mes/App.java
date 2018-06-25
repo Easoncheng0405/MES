2e476461817da85910a5dcc25924484a6bc3d9a0
Easoncheng0405 <597021782@qq.com>
2018-06-25 17:58:27 +0800

    初始化Spring-Boot

--- src/main/java/com/jilin/mes/App.java
+++ src/main/java/com/jilin/mes/App.java
@@ -1,13 +1,26 @@
 package com.jilin.mes;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@Controller
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
     }
 }
