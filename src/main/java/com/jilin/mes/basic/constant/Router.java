51dd5ba17dfad6538189f7777cae660f29d457e7
Easoncheng0405 <597021782@qq.com>
2018-07-03 17:06:14 +0800

    添加模板

---
+++ src/main/java/com/jilin/mes/basic/constant/Router.java
@@ -0,0 +1,16 @@
+package com.jilin.mes.basic.constant;
+
+import com.jilin.mes.basic.controller.IndexController;
+
+/*
+ * 创建时间 2018/7/3
+ * 作者： 程杰
+ * 博客： www.chengjie-jlu.com
+ */
+public class Router {
+
+    /**
+     * @see  IndexController#get()
+     */
+    private static final String LOGIN="login";
+}
